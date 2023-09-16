6b345acdbd020b97f2bcc9bfbc422127e27ad209
Abdallah616 <yassinabdou59@gmail.com>
2023-09-16 13:31:35 -0700

    task solved

---
+++ _printf.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, printed_chars = 0;
+
+	va_start(args, format);
+	if (!format)
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+		if (format[i] == '\0')
+			return (-1);
+		printed_chars += get_specifier(format[i], args);
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			printed_chars++;
+		}
+	}
+	va_end(args);
+	return (printed_chars);
+}
--- main.h
+++ main.h
@@ -11,6 +11,8 @@ int print_int(int num);
 int print_char(char c);
 int print_str(const char *str);
 int print_percent(void);
-int handle_specifier(char specifier, va_list args);
+int print_uint(unsigned int num);
+int print_octal(unsigned int num);
+int get_specifier(char specifier, va_list args);
 
 #endif
