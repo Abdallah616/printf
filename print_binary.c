f7f51a7c7b77667aad7aa0253670e52c1c7a20c6
Mouka-99 <moussaharoon99@gmail.com>
2023-09-17 10:04:30 -0700

    task solved

--- print_binary.c
+++ print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_binary - print num in binary
  *
@@ -8,36 +9,36 @@
 */
 int print_binary(unsigned int num)
 {
-        int num_len = 0, i, leadingzeros = 0;
-        unsigned int temp = num;
-        char *bin_str;
+	int num_len = 0, i, leadingzeros = 0;
+	unsigned int temp = num;
+	char *bin_str;
 
-        if (num == 0)
-        {
-                write(1, "0", 1);
-                return (1);
-        }
-        while (temp != 0)
-        {
-                temp /= 2;
-                num_len++;
-        }
-        bin_str = (char *)malloc(sizeof(unsigned int));
-        if (bin_str == NULL)
-                exit(0);
-        temp = num;
-        for (i = num_len - 1; i >= 0; i--)
-        {
-                bin_str[i] = (temp % 2) + '0';
-                temp /= 2;
-        }
-        for (i = 0; i < num_len; i++)
-        {
-                if (bin_str[i] == '0')
-                        leadingzeros++;
-                else
-                        break;
-        }
-        write(1, bin_str + leadingzeros, num_len - leadingzeros);
-        return (num_len - leadingzeros);
+	if (num == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	while (temp != 0)
+	{
+		temp /= 2;
+		num_len++;
+	}
+	bin_str = (char *)malloc(sizeof(unsigned int));
+	if (bin_str == NULL)
+		exit(0);
+	temp = num;
+	for (i = num_len - 1; i >= 0; i--)
+	{
+		bin_str[i] = (temp % 2) + '0';
+		temp /= 2;
+	}
+	for (i = 0; i < num_len; i++)
+	{
+		if (bin_str[i] == '0')
+			leadingzeros++;
+		else
+			break;
+	}
+	write(1, bin_str + leadingzeros, num_len - leadingzeros);
+	return (num_len - leadingzeros);
 }
