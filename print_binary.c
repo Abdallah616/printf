d69510387debabb5d11aff49dfbcbfeb5c000b97
Abdallah616 <yassinabdou59@gmail.com>
2023-09-18 10:29:16 -0700

    task solved

--- print_binary.c
+++ print_binary.c
@@ -7,10 +7,9 @@
  *
  * Return: length of printed num
 */
-int print_binary(int num);
 int print_binary(int num)
 {
-	int len = 0, i, printed_chars= 0;
+	int len = 0, i, printed_chars = 0;
 	char *buffer = (char *)malloc(LOCAL_BUFFER);
 	unsigned int temp;
 
@@ -21,29 +20,11 @@ int print_binary(int num)
 	{
 		while (temp != 0)
 		{
-			buffer[len++] = (temp % 2)+ '0';
+			buffer[len++] = (temp % 2) + '0';
 			temp /= 2;
 		}
 	}
 	temp = num;
-	if (num < 0)
-	{
-		temp = -num;
-		for (i = len - 1; i > 0; i--)
-		{
-			buffer[i] = (temp % 2) + '0';
-			temp /= 2;
-		}
-	buffer[i] = '-';
-	}
-	else
-	{
-		for (i = len - 1; i >= 0; i--)
-		{
-			buffer[i] = (temp % 2) + '0';
-			temp /= 2;
-		}
-	}
 	for (i = len - 1; i >= 0; i--)
 	{
 		write(1, &buffer[i], 1);
