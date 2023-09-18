4531e1fe3c5d0f4802972be8ae00702df6e15431
Mouka-99 <moussaharoon99@gmail.com>
2023-09-18 12:17:12 -0700

    task solved

--- print_char.c
+++ print_char.c
@@ -2,13 +2,14 @@
 
 /**
  * print_char - Print a character
- * @c: The character to print
+ * @ptr: The list of arguments
  *
  * Return: The number of characters printed
  */
 int print_char(va_list ptr)
 {
 	char ch = va_arg(ptr, int);
+
 	if (ch == '\0')
 		return (0);
 	_putchar(ch);
