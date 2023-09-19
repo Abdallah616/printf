af696571ad244a655a0bd48b937959c9421c6b9a
Mouka-99 <moussaharoon99@gmail.com>
2023-09-19 13:31:40 -0700

    task solved

--- main.c
+++ main.c
@@ -19,9 +19,9 @@ int main(void)
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
 	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");++
-	ui = (unsigned int)INT_MAX + 1024;++
-	_printf("Length:[%d, %i]\n", len, len);++
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);++
 	_printf("Negative:[%d]\n", -762534);++
 	printf("Negative:[%d]\n", -762534);++
