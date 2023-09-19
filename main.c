0c6074e49b45fe7bc8e02987f76f7a32884f8314
Mouka-99 <moussaharoon99@gmail.com>
2023-09-19 13:29:46 -0700

    task solved

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(void)
 	int len2;
 	unsigned int ui;
     void *addr;
-++
+
     addr = (void *)0x7ffe637541f0;++
     _printf("Address:[%p]\n", addr);++
     printf("Address:[%p]\n", addr);++
