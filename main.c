51ebc67ed6418a21bd9726883be4f2eb4005910f
Abdallah616 <yassinabdou59@gmail.com>
2023-09-19 13:40:27 -0700

    task solved

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
-#include <limits.h>
-#include <stdio.h>
-#include "main.h"
-
-/**
+#include <limits.h>++
+#include <stdio.h>++
+#include "main.h"++
+++
+/**++
  * main - Entry point
  *
  * Return: Always 0
  */
 int main(void)
 {
-
 	int len, len3;
 	int len2;
 	unsigned int ui;
@@ -20,7 +19,7 @@ int main(void)
     printf("Address:[%p]\n", addr);
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
+	ui = (unsigned int)INT_MAX + 1024;++
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
