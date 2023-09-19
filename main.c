546c82cf02867743a89b0485d2948e0493abca86
Mouka-99 <moussaharoon99@gmail.com>
2023-09-19 13:32:20 -0700

    task solved

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(void)
 	ui = (unsigned int)INT_MAX + 1024;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);++
+	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);++
 	_printf("Unsigned:[%u]\n", ui);++
 	printf("Unsigned:[%u]\n", ui);++
