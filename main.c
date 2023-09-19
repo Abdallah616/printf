4ff5c455129b6c70ecb99c31b6f244d9f06c5ca4
Mouka-99 <moussaharoon99@gmail.com>
2023-09-19 13:32:52 -0700

    task solved

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main(void)
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);++
+	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);++
 	_printf("Unsigned octal:[%o]\n", ui);++
 	printf("Unsigned octal:[%o]\n", ui);++
